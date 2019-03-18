9f3fb109d2d8c73f9c51c7e87c6be8aeee6d0da1
NikolayDinkov <nikdinkov@abv.bg>
2019-03-18 22:26:55 +0200

    2nd
    Homework 1

--- square_root/main.cpp
+++ square_root/main.cpp
@@ -4,13 +4,22 @@
 
 using namespace std;
 
-double square_root(double S, double n)
+double square_root(double S, int n)
 {
-
+    double x=1, x1=S/2;
+    while(n<=50)
+    {
+        if(n%2!=0)
+            x1=(x+S/x)/2;
+        else
+            x=(x1+S/x1)/2;
+        n++;
+    }
+    return x1;
 }
 
 int main()
 {
-    double S, n;
+    printf("%f\n",square_root(40000, 1));
     return 0;
 }
